92dc05970ec8222f1444e4cb785ce8f4654d2abd
dmlittle <dmlittle@mit.edu>
2014-01-23 22:42:20 -0500

    fix issues with double classes and pre-req

--- js/script.js
+++ js/script.js
@@ -11,8 +11,14 @@ document.addEventListener('mouseup', function (e) {
   
       $.get( "http://student.mit.edu/catalog/search.cgi?search="+selection, function(data) {
         var courseName = $(data).find('blockquote').html();
-        var courseName = replaceAll("src=\"/icns", "src=\"http://student.mit.edu/icns", courseName);        
-        var courseName = courseName.substring(courseName.indexOf('<h3>'), courseName.indexOf('<a name="'));
+        var courseName = replaceAll("src=\"/icns", "src=\"http://student.mit.edu/icns", courseName);   
+        
+        if (courseName.match(/<h3>/g).length == 1) {
+           var courseName = courseName.substring(courseName.indexOf('<h3>')); //
+        } else {
+           var courseName = courseName.substring(courseName.indexOf('<h3>'), courseName.indexOf('<a name="'));
+          };
+
         showTooltip(e.pageX-300, e.pageY, courseName);
       });
     }
