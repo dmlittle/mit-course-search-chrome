b214bf237b176d9436370a1b85f4f4b923f3cbd3
dmlittle <dmlittle@mit.edu>
2014-01-23 19:22:46 -0500

    Initial commit.

---
+++ js/script.js
@@ -0,0 +1,53 @@
+// Add empty tooltipContainer to the top of the page.
+var tooltipDOM = document.createElement('div');
+tooltipDOM.setAttribute('class', 'course_tooltip');
+document.body.appendChild(tooltipDOM);
+
+// Listen to mouseup events to edit/reveal the tooltip information
+document.addEventListener('mouseup', function (e) {
+    var selection = window.getSelection().toString();
+
+    if (/^([1-9]|[1-2][0-9]|CMS|STS|21[AWFHLM]|WGS)\.(S?[\d]{2,3}[x]?|UAT|UAP)$/.test(selection)) {
+  
+      $.get( "http://student.mit.edu/catalog/search.cgi?search="+selection, function(data) {
+        var courseName = $(data).find('blockquote').html();
+        var courseName = replaceAll("src=\"/icns", "src=\"http://student.mit.edu/icns", courseName);        
+        var courseName = courseName.substring(courseName.indexOf('<h3>'));
+        showTooltip(e.pageX-300, e.pageY, courseName);
+      });
+    }
+  }, false);
+
+
+// Hide the tooltip when the the mosue is clicked somewhere on the screen
+document.addEventListener('mousedown', function (e) {
+  tooltipDOM.style.visibility = 'hidden';
+  clearSelection();
+}, false);
+
+
+// Move tooltip to the appropiate location and reveal it
+function showTooltip(mouseX, mouseY, selection) {
+  tooltipDOM.innerHTML = selection;
+  tooltipDOM.style.top = mouseY + 'px';
+  tooltipDOM.style.left = mouseX + 'px';
+  tooltipDOM.style.visibility = 'visible';
+}
+
+
+// Helped Functions...
+function escapeRegExp(str) {
+  return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
+}
+
+function replaceAll(find, replace, str) {
+  return str.replace(new RegExp(escapeRegExp(find), 'g'), replace);
+}
+
+function clearSelection() {
+    if (window.getSelection) {
+        window.getSelection().removeAllRanges();
+    } else if (document.selection) {
+        document.selection.empty();
+    }
+}
